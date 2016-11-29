87fa27d41fbec84cdddac494a1b3697e40b2ac86
Joshua Rogers <git@joshuarogers.net>
2016-11-28 19:00:49 -0600

    Updates metadata and license.

--- src/main/java/net/joshuarogers/chunkymod/ChunkMod.java
+++ src/main/java/net/joshuarogers/chunkymod/ChunkMod.java
@@ -12,7 +12,7 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 public class ChunkMod
 {
     public static final String MODID = "chunkymod";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "0.1";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
